4e70d37fbaab693950c5d35cfa2b607161e5fcff
Bitan Guha Roy <bitanguharoy@gmail.com>
2023-12-23 15:53:10 +0530

    Database annotation added

--- src/main/java/com/bitan/pdtserv/models/Product.java
+++ src/main/java/com/bitan/pdtserv/models/Product.java
@@ -1,14 +1,18 @@
 package com.bitan.pdtserv.models;
 
 import com.sun.jdi.event.StepEvent;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends Base{
 
     private String title,description,imageref;
     private double price;
+    @ManyToOne
     private Category category;
 }
